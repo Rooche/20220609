3ed86f2ca2ab07f180402140a37601ce3d4696ea
admin <admin@YD2-21>
2022-06-14 09:31:32 +0900

    20220614

--- bootstraptest/src/main/java/co/edu/bootstraptest/web/FrontController.java
+++ bootstraptest/src/main/java/co/edu/bootstraptest/web/FrontController.java
@@ -18,31 +18,31 @@ import co.edu.bootstraptest.home.Home;
 public class FrontController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private HashMap<String, Command> map = new HashMap<String, Command>();
-    public FrontController() {
-        super();
-    }
 
+	public FrontController() {
+		super();
+	}
 
 	public void init(ServletConfig config) throws ServletException {
 		// 실행명령 두는 곳
-		map.put("/home.do", new Home());
+		map.put("/home.do", new Home()); // 처음 들어오는 곳 처리
 	}
 
-
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// 요청분석, 실행 및 결과를 리턴하는곳
 		request.setCharacterEncoding("utf-8");
 		String uri = request.getRequestURI();
 		String contextPath = request.getContextPath();
 		String page = uri.substring(contextPath.length());
-		
+
 		Command command = map.get(page);
 		String viewPage = command.exec(request, response);
-		
-		if(!viewPage.endsWith(".do")) {
-			if(!viewPage.startsWith("ajax:")) {
-				//ajax처리
-			}else {	
+
+		if (!viewPage.endsWith(".do")) {
+			if (!viewPage.startsWith("ajax:")) {
+				// ajax처리
+			} else {
 				viewPage = viewPage + ".tiles";
 			}
 		}
